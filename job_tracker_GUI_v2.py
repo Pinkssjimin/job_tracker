ac6ffcdd87415b994e223e7f5277285f1ffcd6c5
Pinkssjimin <jiminahn@pnghs.school.nz>
2022-07-17 21:52:11 +1200

    Update job_tracker_GUI_v2.py

--- job_tracker_GUI_v2.py
+++ job_tracker_GUI_v2.py
@@ -18,10 +18,91 @@ class Start:
         self.job_frame = Frame(bg=background_colour, pady=10)
         self.job_frame.grid()
 
-        # banner image
-        self.logo = PhotoImage(file="job_tracker\logo.GIF")
-        logo_label = Label(self.job_frame, image=self.logo, pady=10, padx=10)
+        # title
+        logo_label = Label(self.job_frame, text="Suzy's Professional Mobile Service",
+                           pady=10, padx=10)
         logo_label.grid(row=0)
+
+        # Job tracker heading (row 1)
+        self.entry_frame = LabelFrame(text="Add New Job",
+                                       font="Arial 19 bold")
+        self.entry_frame.grid(row=1, padx=10, pady=10)
+
+        # variables for entry inputs
+        self.name_var = StringVar()
+        self.dist_var = StringVar()
+        self.time_var = StringVar()
+        self.wof_var = BooleanVar()
+
+        # name entry (row 2)
+        # name entry title
+        self.name_entry_label = Label(self.entry_frame, text="Customer Name: ",
+                                      font="Arial 14 bold")
+        self.name_entry_label.grid(row=1, column=0)
+
+        # Name Entry Box
+        self.name_entry = Entry(self.entry_frame, textvariable=self.name_var, font="Arial 14")
+        self.name_entry.grid(row=1, column=1, columnspan=2, padx=10, pady=10)
+
+        # job number entry (row 3)
+
+        # distance entry  (row 4)
+        # distance entry title
+        self.dist_entry_label = Label(self.entry_frame, text="Distance Travelled: ",
+                                      font="Arial 14 bold")
+        self.dist_entry_label.grid(row=2, column=0, padx=10, pady=10)
+
+        # distance Entry Box
+        self.dist_entry = Spinbox(self.entry_frame, from_=0, to=1600, textvariable=self.dist_var, width=5)
+        self.dist_entry.grid(row=2, column=1)
+
+        # scale label
+        self.dist_scale = Label(self.entry_frame, text="km", font="Arial 14", justify=LEFT)
+        self.dist_scale.grid(row=2, column=2)
+
+        # Virus protection entry (row 5)
+        # virus protection entry title
+        self.virus_entry_label = Label(self.entry_frame, text="Virus Protection Time: ",
+                                      font="Arial 14 bold")
+        self.virus_entry_label.grid(row=3, column=0, pady=10, padx=10)
+
+        # virus protection Entry Box
+        self.virus_entry = Spinbox(self.entry_frame, from_=0, to=360, textvariable=self.time_var, width=5)
+        self.virus_entry.grid(row=3, column=1, pady=10)
+
+        # scale label (minutes)
+        self.virus_scale = Label(self.entry_frame, text="min", font="Arial 14")
+        self.virus_scale.grid(row=3, column=2)
+
+        # WOF and tune entry (row 6)
+        # wof entry title
+        self.wof_entry_label = Label(self.entry_frame, text="WoF & Tune: ",
+                                      font="Arial 14 bold")
+        self.wof_entry_label.grid(row=4, column=0, padx=10)
+
+        # WoF Entry Box
+        self.wof_checkbox = Checkbutton(self.entry_frame, text="yes", variable=self.wof_var)
+        self.wof_checkbox.grid(row=4, column=1, columnspan=2)
+
+        # Save button (row 7)
+        self.save_btn = Button(text="Save",
+                               command=lambda: self.to_display())
+        self.save_btn.grid(row=5, pady=10)
+
+    def to_display(self):
+        customer_name = self.name_var.get()
+        dist = float(self.dist_var.get())
+        virus_protection = float(self.time_var.get())
+        wof_tune = self.wof_var.get()
+
+        Display(self, customer_name, dist, virus_protection, wof_tune)
+
+class Display:
+    def __init__(self, partner, customer_name, distance, virus_protection, wof_tune):
+        print(customer_name, distance, virus_protection, wof_tune)
+
+        # partner allows us to disable the button and not spawn multiple boxes
+        partner.save_btn.config(state=DISABLED)
 
 # main routine
 if __name__ == "__main__":
